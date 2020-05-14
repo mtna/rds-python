3ce2fb5e54b369890315e22a9e2aa14bb3ab9a0e
smlucas1993 <64433267+smlucas1993@users.noreply.github.com>
2020-05-14 12:25:14 -0400

    docs(setup): fixed version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rds-library",
+    name="rds",
     version="v0.1.0-alpha.0",
     author="Metadata Technology North America Inc.",
     author_email="mtna@mtna.us",
