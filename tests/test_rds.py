6287a4f909262947ccaa4baaa7ac226597eda6c5
Sean Lucas <sean.lucas@mtna.us>
2020-05-21 16:00:37 -0400

    docs(0.1.2): commented out tests
    tests have been disabled until tab engine is public

--- tests/test_rds.py
+++ tests/test_rds.py
@@ -6,41 +6,42 @@ import sys, os
 sys.path.insert(0, os.path.abspath('..'))
 from rds import DataProduct
 
-def test_select():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    results = dp.select(cols=['sex', 'sum(cnt_death)'], where='sex=male', orderby=['sum(cnt_death)'], groupby=['sex'])
-    assert results.records is not None
-    assert results.columns is not None
-    assert results.metadata is not None
 
-def test_tabulate():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    results = dp.tabulate(dims=['sex'], measure=['deaths:sum(cnt_death)'])
-    assert results.records is not None
-    assert results.columns is not None
-    assert results.metadata is not None
+#def test_select():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    results = dp.select(cols=['sex', 'sum(cnt_death)'], where='sex=male', orderby=['sum(cnt_death)'], groupby=['sex'])
+#    assert results.records is not None
+#    assert results.columns is not None
+#    assert results.metadata is not None
 
-def test_metadata_catalog():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    metadata = dp.catalog()
-    assert len(metadata) > 0
+#def test_tabulate():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    results = dp.tabulate(dims=['sex'], measure=['deaths:sum(cnt_death)'])
+#    assert results.records is not None
+#    assert results.columns is not None
+#    assert results.metadata is not None
 
-def test_metadata_variables():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    metadata = dp.variable()
-    assert len(metadata) > 0
+#def test_metadata_catalog():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    metadata = dp.catalog()
+#    assert len(metadata) > 0
+
+#def test_metadata_variables():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    metadata = dp.variable()
+#    assert len(metadata) > 0
     
-def test_metadata_profile():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    metadata = dp.profile('sex')
-    assert len(metadata) > 0
+#def test_metadata_profile():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    metadata = dp.profile('sex')
+#    assert len(metadata) > 0
     
-def test_metadata_classification():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    metadata = dp.classification('sex')
-    assert len(metadata) > 0
+#def test_metadata_classification():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    metadata = dp.classification('sex')
+#    assert len(metadata) > 0
     
-def test_metadata_code():
-    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
-    metadata = dp.code('sex')
-    assert len(metadata) > 0
+#def test_metadata_code():
+#    dp = DataProduct('https://covid19.richdataservices.com', 'us_oh', 'oh_doh')
+#    metadata = dp.code('sex')
+#    assert len(metadata) > 0
